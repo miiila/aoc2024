577a67b7021b0964528c69f403d323d2b37513b7
Mila Votradovec <me@milavotradovec.cz>
2024-12-01 18:18:43 +0100

    day 1 - slightly better

--- day1.exs
+++ day1.exs
@@ -1,26 +1,29 @@
-Code.require_file("aoc.ex")
-
 import AocHelper
 
 defmodule Day1 do
   def solve_part_1(input) do
     [first, second] = input
-    Enum.zip(Enum.sort(first), Enum.sort(second))
-    |> Enum.map(fn {f, s} -> f - s end)
-    |> Enum.map(&abs/1)
+
+    first
+    |> Enum.sort()
+    |> Enum.zip(Enum.sort(second))
+    |> Enum.map(fn {f, s} -> abs(f - s) end)
     |> Enum.sum()
   end
 
   def solve_part_2(input) do
     [first, second] = input
-    Enum.map(first, fn x -> x * Enum.count(for y <- second, y == x, do: y) end) |> Enum.sum
+    second_freq = Enum.frequencies(second)
+    Enum.reduce(first, 0, fn x, acc -> acc + x * Map.get(second_freq, x, 0) end)
   end
 end
 
 input = load_input(1, false, fn l -> String.split(l, "   ") |> Enum.map(&String.to_integer/1) end)
-input = Enum.reduce(input, [[], []], fn [f | [s | _]], [acc_f, acc_s] ->
-        [[f | acc_f], [s | acc_s]]
-      end)
+
+input =
+  Enum.reduce(input, [[], []], fn [f, s], [acc_f, acc_s] ->
+    [[f | acc_f], [s | acc_s]]
+  end)
 
 IO.puts(Day1.solve_part_1(input))
 
