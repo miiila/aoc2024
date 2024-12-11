2987311984d13fa7dec560ef842c58e288f3302b
Miloslav Votradovec <miloslav.votradovec@second-foundation.eu>
2024-12-11 16:28:22 +0100

    day 11 - part 2 original

--- day11.exs
+++ day11.exs
@@ -17,13 +17,34 @@ defmodule Day11 do
     end
   end
 
-  defp solve_1(input, 75), do: length(input)
+  defp solve_1(input, 25), do: length(input)
 
   defp solve_1(input, c) do
     Enum.map(input, &change_stone/1) |> List.flatten |> solve_1(c+1)
   end
-  
 
+  defp solve_2(i, 0, mem) do 
+    {1, Map.put(mem, i, Map.put(Map.get(mem, i, %{}), 0, 1))}
+  end
+
+  defp solve_2(i, gen, mem) do 
+    r = Map.get(Map.get(mem, i, %{}), gen, nil)
+    case r do 
+      nil -> 
+        {rr,mem} = case k = change_stone(i) do 
+          [k1, k2] ->
+            {r1, mem}  = solve_2(k1, gen - 1, mem)
+            {r2, mem} = solve_2(k2, gen - 1, mem)
+            {r1 + r2,mem}
+          kk -> 
+            solve_2(kk, gen - 1, mem)
+        end
+        mem = Map.put(mem, i, Map.put(Map.get(mem, i, %{}), gen, rr))
+        {rr, mem} 
+      r -> 
+        {r, mem}
+    end
+  end
 
 
   def solve_part_1(input) do
@@ -31,7 +52,10 @@ defmodule Day11 do
   end
 
   def solve_part_2(input) do
-
+    {a,_} = Enum.map_reduce(input, %{}, fn k, acc ->
+      solve_2(k, 75, acc) 
+    end)
+    Enum.sum(a)
   end
 end
 
